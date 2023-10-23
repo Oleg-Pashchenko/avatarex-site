1fe0c8bc10b23b39f533d87e309fe3aff8d3fef9
olehandro <odpash.itmo@gmail.com>
2023-10-23 19:51:49 +0300

    updated db-mode-view

--- home/forms.py
+++ home/forms.py
@@ -19,23 +19,23 @@ class GptDefaultMode(forms.Form):
         (False, 'passive')
     )
 
-    context = forms.CharField(widget=forms.Textarea(attrs={'class': 'rounded-value',
+    context = forms.CharField(widget=forms.Textarea(attrs={'class': 'rounded-value blacked',
                                                            'placeholder': 'Введите ваш контекст запроса'
                                                            }))
     max_tokens = forms.IntegerField(
-        min_value=100, required=True, widget=forms.NumberInput(attrs={'class': 'rounded-value',
+        min_value=100, required=True, widget=forms.NumberInput(attrs={'class': 'rounded-value blacked',
                                                                       'placeholder': 'от 100 токенов'
                                                                       })
     )
     temperature = forms.FloatField(
-        min_value=0, max_value=2, required=True, widget=forms.NumberInput(attrs={'class': 'rounded-value',
+        min_value=0, max_value=2, required=True, widget=forms.NumberInput(attrs={'class': 'rounded-value blacked',
                                                                                  'placeholder': 'от 0 до 2'
                                                                                  }
                                                                           ))
     voice_message_detection = forms.ChoiceField(required=True, choices=VOICE_CHOICES,
 
-                                                widget=forms.Select(attrs={'class': 'rounded-value'}))
+                                                widget=forms.Select(attrs={'class': 'rounded-value blacked'}))
     model = forms.ChoiceField(choices=CONTEXT_CHOICES, required=True,
-                              widget=forms.Select(attrs={'class': 'rounded-value'}))
-    fine_tunel_model_id = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'rounded-value',
+                              widget=forms.Select(attrs={'class': 'rounded-value blacked'}))
+    fine_tunel_model_id = forms.CharField(required=False, widget=forms.TextInput(attrs={'class': 'rounded-value blacked',
                                                                                         'placeholder': 'Если вы хотите использовать собственную модель укажите ее ID'}))
